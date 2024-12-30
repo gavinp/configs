8b3c8f5ecd7766436d94208c41929557bce3f6dd
Gavin Peters <gavin@ytz.ca>
2024-12-30 17:58:43 -0500

    the basics

---
+++ _emacs.d/init.el
@@ -0,0 +1,24 @@
+;; Gavin's init.el file.
+
+;; In github, no less.
+
+(xterm-mouse-mode)
+(setq visible-bell t)
+(menu-bar-mode 0)
+(setq scroll-bar-mode nil)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(manoj-dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
